213c759c29c4784ee9ac3ca4361e2e945669f1ae
dzabu98 <103649761+dzabu98@users.noreply.github.com>
2022-07-29 20:43:47 +0200

    Add files via upload

--- src/main/java/group/revealed/steps/StepDefinitions.java
+++ src/main/java/group/revealed/steps/StepDefinitions.java
@@ -42,4 +42,49 @@ public class StepDefinitions {
         base.getContactUsPage().checkTitle();
         base.getContactUsPage().openHours();
     }
+
+    @Given("^I navigate to SportsDirect homepage$")
+    public void i_navigate_to_Sports_Direct_homepage()
+    {
+        // Write code here that turns the phrase above into concrete actions
+        base.getDriver().get("https://rs.sportsdirect.com/");
+    }
+
+    @Given("^I click on SALE tab in top menu$")
+    public void i_click_on_SALE_tab_in_top_menu() throws InterruptedException
+    {
+        base.getSDirectHomePage().clickOnSaleTab();
+    }
+
+
+    @Given("^Page url contains the word sale$")
+    public void page_url_contains_the_word_sale()
+    {
+        base.getSDirectSalePage().pageUrlCheck();
+    }
+
+    @Given("^I can see Nike logo inside of POPULAR SALE BRANDS tab$")
+    public void i_can_see_Nike_logo_inside_of_POPULAR_SALE_BRANDS_tab()
+    {
+        base.getSDirectSalePage().nikeLogoPresenceCheck();
+    }
+
+    @When("^I click on Nike logo of Sale page$")
+    public void i_click_on_Nike_logo_of_Sale_page() throws InterruptedException
+    {
+        base.getSDirectSalePage().clickOnNikeLogo();
+    }
+
+    @Then("Page url should contain word sale and word nike")
+    public void page_url_should_contain_word_sale_and_word_nike()
+    {
+        base.getSDirectNikeSalePage().pageUrlCheck();
+    }
+
+    @Then("I should see the title NIKE SALE on top of the page")
+    public void i_shoul_see_the_title_NIKE_SALE_on_top_of_the_page()
+    {
+        base.getSDirectNikeSalePage().pageNameCheck();
+    }
+
 }
